6e42853553ab8a3507b4211418889cbe2db53299
Maksim Bartoshyk <maksim_bartoshyk@epam.com>
2025-09-18 18:03:54 +0300

    feat (spg): fix render issues
    - scroll only up/down with time axis
    - fix time axis ticks rendering
    - implement gestures for binning and scale switch
    - add color palettes

--- app/src/main/java/org/fe57/atomspectra/AtomSpectraSpectrogram.java
+++ app/src/main/java/org/fe57/atomspectra/AtomSpectraSpectrogram.java
@@ -20,10 +20,13 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Locale;
 
-public class AtomSpectraSpectrogram extends Activity {
+public class AtomSpectraSpectrogram extends Activity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
     private static boolean isActive = false;
     private static int sbin = 1;
     private static String scale = AtomSpectraSpectrogramView.SCALE_SQRT;
+    private static String palette = AtomSpectraSpectrogramView.PALETTE_IRON;
+    private GestureDetector gestureDetector;
+    private ScaleGestureDetector scaleGestureDetector;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -42,6 +45,7 @@ public class AtomSpectraSpectrogram extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atom_spectra_spectrogram);
+        // action bar
         ActionBar bar = getActionBar();
         if (bar != null) {
             bar.setDisplayShowHomeEnabled(true);
@@ -49,7 +53,7 @@ public class AtomSpectraSpectrogram extends Activity {
         }
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-
+        // intents
         final IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Constants.ACTION.ACTION_CLOSE_SPECTROGRAM);
         intentFilter.addAction(Constants.ACTION.ACTION_SPECTROGRAM_UPDATED);
@@ -59,50 +63,40 @@ public class AtomSpectraSpectrogram extends Activity {
             registerReceiver(mDataUpdateReceiver, intentFilter);
         }
 
-        // validate sbin and scale
+        // validate sbin and scale, init cintrol panel
         if (sbin < 1) {
             sbin = 1;
         }
         if (sbin > 128) {
             sbin = 128;
         }
-
         HashSet<String> allowedScale = new HashSet<>(Arrays.asList(AtomSpectraSpectrogramView.SCALE_SQRT, AtomSpectraSpectrogramView.SCALE_LOG, AtomSpectraSpectrogramView.SCALE_LIN));
         if (!allowedScale.contains(scale)) {
             scale = AtomSpectraSpectrogramView.SCALE_SQRT;
         }
-
+        HashSet<String> allowedPalette = new HashSet<>(Arrays.asList(AtomSpectraSpectrogramView.PALETTE_IRON, AtomSpectraSpectrogramView.PALETTE_LIME, AtomSpectraSpectrogramView.PALETTE_YELLOW, AtomSpectraSpectrogramView.PALETTE_GLOW, AtomSpectraSpectrogramView.PALETTE_GRAY));
+        if (!allowedPalette.contains(palette)) {
+            palette = AtomSpectraSpectrogramView.PALETTE_IRON;
+        }
         updateControlPanel();
+
+        // gestures
+        gestureDetector = new GestureDetector(this, this);
+        gestureDetector.setOnDoubleTapListener(this);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+        findViewById(R.id.viewSpectrogram).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                scaleGestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onBinPlusClick(View view) {
-        if (sbin < 128) {
-            sbin *= 2;
-
-            updateControlPanel();
-            updateSpectrogram(false);
-        }
-    }
-
-    public void onBinMinusClick(View view) {
-        if (sbin > 1) {
-            sbin /= 2;
-
-            updateControlPanel();
-            updateSpectrogram(false);
-        }
-    }
-
-    public void onScaleClick(View view) {
+    public boolean onDoubleTap(MotionEvent e) {
+        // update scale
         switch (scale) {
             case AtomSpectraSpectrogramView.SCALE_LIN:
                 scale = AtomSpectraSpectrogramView.SCALE_SQRT;
@@ -119,6 +113,17 @@ public class AtomSpectraSpectrogram extends Activity {
 
         updateControlPanel();
         updateSpectrogram(false);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private final BroadcastReceiver mDataUpdateReceiver = new BroadcastReceiver() {
@@ -168,7 +173,7 @@ public class AtomSpectraSpectrogram extends Activity {
                     channelBin = 2;
                 }
 
-                spgView.renderSpectrogram(AtomSpectraSpectrogramData.instance, sbin, channelBin, scale, scrollToBottom);
+                spgView.renderSpectrogram(AtomSpectraSpectrogramData.instance, sbin, channelBin, scale, palette, scrollToBottom);
             }
 
             TextView rowCount = findViewById(R.id.textViewRowCount);
@@ -185,9 +190,40 @@ public class AtomSpectraSpectrogram extends Activity {
             textSpectrumBin.setText(String.format("↕bin:%dx", sbin));
         }
 
+        TextView textPalette = findViewById(R.id.textViewSpgPalette);
+        if (textPalette != null) {
+            textPalette.setText(palette);
+        }
+
         TextView textScale = findViewById(R.id.textViewSpgScale);
         if (textScale != null) {
             textScale.setText(scale);
+        }
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScaleEnd(ScaleGestureDetector detector) {
+            double factor = detector.getScaleFactor();
+            if (factor > 1) {
+              if (sbin > 1) {
+                sbin /= 2;
+
+                updateControlPanel();
+                updateSpectrogram(false);
+              }
+            }
+
+            if (factor < 1) {
+              if (sbin < 128) {
+                sbin *= 2;
+
+                updateControlPanel();
+                updateSpectrogram(false);
+              }
+            }
+
+            return true;
         }
     }
 }
